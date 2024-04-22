f8a1475258bfd7b46a7b6b09586eb2d686938f09
chetankumar8919@gmail.com <chetan@Chetans-MacBook-Pro.local>
2024-04-22 16:27:56 -0400

    Initial commit of Resort Management Web App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import {Route, Switch} from "react-router-dom";
+
+function App(){
+  return (<>
+  <Route path = "/" Component={Home}/>
+  <Route path = "/rooms" Component={Rooms}/>
+  <Route path = "/single-room" Component={SingleRoom}/>
+  
+  </>);
+
 }
+  
 
 export default App;
